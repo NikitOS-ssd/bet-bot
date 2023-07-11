908ec70fa739ba64c368aa7b1227b550f88a206d
Nikita Matskovsky <n.matskovsky@octogroup.io>
2023-07-11 11:53:21 +0300

    Create start command

--- bot/bot_commands/index.js
+++ bot/bot_commands/index.js
@@ -1,3 +1,11 @@
-async function startCommand(ctx) {
-	const startMessage = `Привет, ${ctx.message.from.first_name}!`
+async function Start(ctx) {
+	const response = `Приветствую, ${ctx.message.from.first_name}!
+	${CommandsMessages.StartMessageForUser.message1}`
+	
+		await ctx.reply(response)
+	
+		await ctx.replyWithPhoto(`${ClinicObj.image}`)
+	
+		await ctx.reply(CommandsMessages.StartMessageForUser.message2, Keyboard.startKeyboard)
+	
 }
