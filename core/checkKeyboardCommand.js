da033a1f6b47e046ea5df8efe8d6e604dc75d3a1
Nikita Matskovsky <n.matskovsky@octogroup.io>
2023-07-24 13:57:56 +0400

    Updates

--- core/checkKeyboardCommand.js
+++ core/checkKeyboardCommand.js
@@ -6,6 +6,24 @@ const { translate } = require("./localizations/translate");
 
 const LANG = process.env.USER_LANG;
 
+const INFO_TEXT = `
+OUR MISSION
+
+BWANA BET is here to help your DREAMS COME TRUE! Our mission is to give a chance to every player to BET LIKE A BOSS and allow each and every one the chance to make a life changing BET TODAY and wake up a MILLIONAIRE TOMORROW!
+
+BWANA BET is here to help your DREAMS COME TRUE!
+
+Our mission is to give a chance to every player to BET LIKE A BOSS and allow each and every one the chance to make a life changing BET TODAY and wake up a MILLIONAIRE TOMORROW!
+
+BWANA BET is a proudbly Zambian sports betting company, made for Zambians! Our aim to make every player a BIG BWANA! WIN BIG with our MASSIVE ODDS and MAXIMUM PAYOUT of K2,000,000! ; Join the winning team of Bwana Bet players and enjoy the best that you can get in the world of sports betting. Bet on local and international teams, enjoy early CASHOUT, boosted ODDS and ONLY BWANABET gives you a MASSIVE BONUS of up to 1000% on your tickets! Our aim is to help Zambians achieve their dreamsthrough responsible betting.
+
+BWANA BET is a proudbly Zambian sports betting company, made for Zambia!
+
+Our aim to make every player a BIG BWANA
+
+WIN BIG with our MASSIVE ODDS and MAXIMUM PAYOUT of K2,000,000!
+`
+
 function checkKeyboardCommand(message) {
   try {
     if (!message) {
@@ -53,7 +71,7 @@ function setKeyboardForButtonCommand(ctx, settingsName) {
       case 'bonus':
         return ctx.reply("Ваши бонусы")
       case 'info':
-        return ctx.reply("Информация")
+        return ctx.reply(INFO_TEXT)
       default:
         return ctx.reply("None. 404")
     }
